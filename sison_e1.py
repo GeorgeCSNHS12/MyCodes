be3cad1389df233f2a9156fa74d15203315edf00
GeorgeCSNHS12 <43868130+GeorgeCSNHS12@users.noreply.github.com>
2018-10-07 23:58:51 +0800

    Add files via upload
    Work Immersion Assignment

---
+++ sison_e1.py
@@ -0,0 +1,10 @@
+data = { 
+    "myName" : "George William M. Sison",
+    "mySpiritAnimal" : "Cat",
+    "myReason" : "I'm an apathetic person and energy converver just like a cat",
+    "myHobby" : "play MMORPGs and watch animes",
+    "myFutureProfession" : "Software Developer"
+}
+
+print("I am {}.".format(data["myName"]), "My spirit animal is {},".format(data["mySpiritAnimal"]), "because {}.".format(data["myReason"]), sep = "\n")
+print("When not in school, I love to {}.".format(data["myHobby"]), "I dream of being a/an {} in the future.".format(data["myFutureProfession"]), sep = "\n")

ca20b59a13e0d7822370e5c04e2667e7e3306c44
GeorgeCSNHS12 <43868130+GeorgeCSNHS12@users.noreply.github.com>
2018-10-08 09:28:10 +0800

    Update Work Immersion Assignment

--- sison_e2.py
+++ sison_e2.py
@@ -1,3 +1,8 @@
+# filename      : sison_e2.py
+# author        : George William Sison
+# description   : This is a python program that classifies a tropical cyclone
+#                 with its sustained winds.
+
 import sys
 
 windIntensity = float(sys.argv[1])
@@ -10,5 +15,5 @@ elif windIntensity >= 89 and windIntensity < 118 :
     print("Severe Tropical Storm")
 elif windIntensity >= 62 and windIntensity < 89 : 
     print("Tropical Storm")
-elif windIntensity <= 61 :
+elif windIntensity <= 61 and windIntensity > 0 :
     print("Tropical Depression")

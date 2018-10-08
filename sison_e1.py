aef16a23586032891f3c52f587dbc06c74481947
GeorgeCSNHS12 <43868130+GeorgeCSNHS12@users.noreply.github.com>
2018-10-08 09:27:35 +0800

    Update Work Immersion Assignment

--- sison_e1.py
+++ sison_e1.py
@@ -1,10 +1,22 @@
+# filename      : sison_e1.py
+# author        : George William Sison
+# description   : This is a python program that prints contents of a dictionary
+#                 in a specific format.
+
 data = { 
-    "myName" : "George William M. Sison",
-    "mySpiritAnimal" : "Cat",
-    "myReason" : "I'm an apathetic person and energy conserver just like a cat",
-    "myHobby" : "play MMORPGs and watch animes",
-    "myFutureProfession" : "Software Developer"
+        "myName" : "George William M. Sison",
+        "mySpiritAnimal" : "Cat",
+        "myReason" : "I'm an apathetic person and energy conserver just like a cat",
+        "myHobby" : "play MMORPGs and watch Animes",
+        "myFutureProfession" : "Software Developer"
 }
 
-print("I am {}.".format(data["myName"]), "My spirit animal is {},".format(data["mySpiritAnimal"]), "because {}.".format(data["myReason"]), sep = "\n")
-print("When not in school, I love to {}.".format(data["myHobby"]), "I dream of being a/an {} in the future.".format(data["myFutureProfession"]), sep = "\n")
+message = "I am {}.\n" \
+        "My spirit animal is {},\n" \
+        "because {}.\n" \
+        "When not in school, I love to {}.\n" \
+        "I dream of being a/an {} in the future."
+
+print(message.format(data["myName"], data["mySpiritAnimal"], data["myReason"], data["myHobby"], data["myFutureProfession"]))
+#print("I am {}.".format(data["myName"]), "My spirit animal is {},".format(data["mySpiritAnimal"]), "because {}.".format(data["myReason"]), sep = "\n")
+#print("When not in school, I love to {}.".format(data["myHobby"]), "I dream of being a/an {} in the future.".format(data["myFutureProfession"]), sep = "\n")

5b54db8dce66f801c930716aaf9e90f5c7e55319
GeorgeCSNHS12 <43868130+GeorgeCSNHS12@users.noreply.github.com>
2019-07-11 11:48:40 +0800

    Add files via upload

---
+++ try.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#define print cout<<
+#define input cin>>
+
+using namespace std;
+
+void getBMI(){
+    double weight, height, bmi;
+
+    print "\nInput Weight(kg): ";
+    input weight;
+    print "\nInput Height(m): ";
+    input height;
+
+    bmi = weight / (height * height);
+
+    print "\nYour BMI: [" << bmi << "]\n";
+
+    print "\nTable Values\nUnderweight:\t18.5 Below\nNormal:\t\tbetween 18.5 and 24.9\nOverweight:\t25 and 29.9\nObese:\t\tAbove 30\n";
+
+}
+
+void getGasMileage(){
+    double miles, gas, mpg, tMiles, tGas, tMpg;
+    int ctr = 1, dec;
+
+    do{
+        print "\nInput Miles in Trip #" << ctr << ": ";
+        input miles;
+        print "\nInput Gas in Trip #" << ctr << ": ";
+        input miles;
+
+    }while(dec == 0);
+
+
+}
+
+int main(){
+    int dec;
+    print "\n(0) for BMI and (1) for Gas Mileage\nYour Input: ";
+    input dec;
+
+    dec == 0 ? getBMI() : getGasMileage();
+    return 0;
+}
